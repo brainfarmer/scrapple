e476204a07d16b4d577ef15ce58271c8ea776271
Dan Nemeth <dnemeth@pivotal.io>
2014-10-17 17:05:24 -0400

    Removed legacy MarkdownController and associated routes

--- web/router.ex
+++ web/router.ex
@@ -2,7 +2,6 @@ defmodule Scrapple.Router do
   use Phoenix.Router
 
   get "/", Scrapple.PageController, :index, as: :home
-  get "/markdown/:page", Scrapple.MarkdownController, :show
   get "/docs/:doc", Scrapple.DocumentController, :index, as: :docs
   get "/docs/:doc/*path", Scrapple.DocumentController, :show, as: :page
 
